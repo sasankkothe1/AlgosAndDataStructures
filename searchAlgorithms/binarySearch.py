c1a3a6849689e77b3f102ac870738a25d58d3bc3
Venkata Kothe <ga53xil@mytum.de>
2022-08-16 21:34:29 +0200

    added explanatory comments

--- searchAlgorithms/binarySearch.py
+++ searchAlgorithms/binarySearch.py
@@ -3,15 +3,22 @@ def binarySearch(elements, search_el):
     low = 0
     high = n_elements - 1
 
+    # iteration continues as soon as low and high converge or equal
     while low <= high:
+        # always calculate mid
         mid = (low + high) // 2
         if elements[mid] == search_el:
+            # if search el is at mid, return mid
             return mid
         elif elements[mid] > search_el:
+            # if seach el is less than mid, then probably element is
+            # present in the lower half. So high will be 1 less than mid
             high = mid - 1
         elif elements[mid] < search_el:
+            # if seach el is greater than mid, then probably element is
+            # present in the upper half. So low will be 1 greater than mid
             low = mid + 1
-    return -1
+    return -1  # if element is not found
 
 
 def main():
